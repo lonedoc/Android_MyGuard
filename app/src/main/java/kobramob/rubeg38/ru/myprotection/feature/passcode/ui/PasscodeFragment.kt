0c8f45ede4929d1ea6b78a34ef17efba59f152a4
Aleksey Get <aleksey.gett@gmail.com>
2022-02-09 11:17:33 +0800

    Create the passcode screen layout

--- app/src/main/java/kobramob/rubeg38/ru/myprotection/feature/passcode/ui/PasscodeFragment.kt
+++ app/src/main/java/kobramob/rubeg38/ru/myprotection/feature/passcode/ui/PasscodeFragment.kt
@@ -1,8 +1,9 @@
 package kobramob.rubeg38.ru.myprotection.feature.passcode.ui
 
 import androidx.fragment.app.Fragment
+import kobramob.rubeg38.ru.myprotection.R
 
-class PasscodeFragment : Fragment() {
+class PasscodeFragment : Fragment(R.layout.fragment_passcode) {
 
     companion object {
         fun create(): PasscodeFragment = PasscodeFragment()
