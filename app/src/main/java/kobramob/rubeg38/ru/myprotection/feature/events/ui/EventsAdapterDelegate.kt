85bdfe2c109c0f19268b520fa142459fa11dfd84
Aleksey Get <aleksey.gett@gmail.com>
2022-06-16 16:49:58 +0800

    Fix code-style in the EventsAdapterDelegate.kt

--- app/src/main/java/kobramob/rubeg38/ru/myprotection/feature/events/ui/EventsAdapterDelegate.kt
+++ app/src/main/java/kobramob/rubeg38/ru/myprotection/feature/events/ui/EventsAdapterDelegate.kt
@@ -1,6 +1,8 @@
 package kobramob.rubeg38.ru.myprotection.feature.events.ui
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.InsetDrawable
 import androidx.appcompat.content.res.AppCompatResources
 import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegateViewBinding
@@ -10,6 +12,8 @@ import kobramob.rubeg38.ru.myprotection.feature.events.domain.models.Event
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val ICON_INSET = 32
+
 fun eventsAdapterDelegate() =
     adapterDelegateViewBinding<Event, Event, EventsItemBinding>(
         viewBinding = { layoutInflater, parent ->
@@ -18,11 +22,9 @@ fun eventsAdapterDelegate() =
     ) {
         bind {
             val backgroundColor = context.getColor(getColorResByType(item.type))
-            val iconDrawableRes = getIconResByType(item.type)
-            val iconDrawable = AppCompatResources.getDrawable(context, iconDrawableRes)
-            val drawable = InsetDrawable(iconDrawable, 32)
+            val iconDrawable = getIconByType(context, item.type)
 
-            binding.iconImageView.setImageDrawable(drawable)
+            binding.iconImageView.setImageDrawable(iconDrawable)
             binding.iconImageView.setBackgroundColor(backgroundColor)
 
             binding.descriptionTextView.text = item.description
@@ -31,7 +33,7 @@ fun eventsAdapterDelegate() =
         }
     }
 
-fun getColorResByType(type: Int) = when(type) {
+private fun getColorResByType(type: Int) = when(type) {
     1, 2, 3, 4, 77, 79, 81, 85 -> R.color.deep_orange_500
     5, 33 -> R.color.brown_500
     10, 68, 69 -> R.color.green_500
@@ -39,7 +41,14 @@ fun getColorResByType(type: Int) = when(type) {
     else -> R.color.gray_500
 }
 
-fun getIconResByType(type: Int) = when(type) {
+private fun getIconByType(context: Context, type: Int): Drawable {
+    val iconRes = getIconResByType(type)
+    val iconDrawable = AppCompatResources.getDrawable(context, iconRes)
+    return InsetDrawable(iconDrawable, ICON_INSET)
+}
+
+
+private fun getIconResByType(type: Int) = when(type) {
     1, 2, 3, 77, 79, 85 -> R.drawable.status_alarm_icon
     4 -> R.drawable.fire_alarm_icon
     5 -> R.drawable.status_malfunction_icon
@@ -50,5 +59,5 @@ fun getIconResByType(type: Int) = when(type) {
 }
 
 @SuppressLint("SimpleDateFormat")
-fun getTimestampStr(timestamp: Date): String =
+private fun getTimestampStr(timestamp: Date): String =
     SimpleDateFormat("dd.MM.yyyy hh:mm:ss").format(timestamp)
